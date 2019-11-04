d5fcae347fc5d0737240f7f756cc0e263841d4cf
izzaturrahman19 <55430574+izzaturrahman19@users.noreply.github.com>
2019-11-04 17:10:56 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,33 +2,16 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"strconv"
 	"net/http"
 	"html/template"
 	"github.com/gorilla/mux"
     "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
-}
-
-type allEvents []event
-
-var events = allEvents{
-	{
-		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
-	},
-}
 
 var ctx= context.Background()
 
@@ -68,7 +51,30 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "", http.StatusBadRequest)
 }
 
+func readEvent(w http.ResponseWriter, r *http.Request) {
+	if r.Method== "GET"{
+
+		var tmpl = template.Must(template.New("form").ParseFiles("view.html"))
+		var err = tmpl.Execute(w, nil)
+
+		if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return
+	}
+	http.Error(w, "", http.StatusBadRequest)
+}
+
 func createEvent(w http.ResponseWriter, r *http.Request) {
+	if r.Method== "GET"{
+		var tmpl = template.Must(template.New("form").ParseFiles("view.html"))
+		var err = tmpl.Execute(w, nil)
+
+		if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return
+	}
 	
 	if r.Method== "POST"{
 
@@ -86,13 +92,11 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 
 	var name = r.FormValue("name")
 	var bubble = r.Form.Get("grade")
-	var grade int
-	if grade, err := strconv.ParseInt(bubble, 10, 32); err == nil {
+	grade, err := strconv.Atoi(bubble)
+	if err == nil {
 		fmt.Printf("%T, %v\n", grade, grade)
 	}
 	var category = r.Form.Get("category")
-	
-	
 	var data = map[string]string{"name": name, "category": category }
 	var angka = map[string]int{ "grade": grade}
 	
@@ -108,57 +112,120 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 
-}}
-
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
-		}
 	}
+	http.Error(w, "", http.StatusBadRequest)
 }
 
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(events)
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	
+	if r.Method == "GET"{
+		
+		var tmpl = template.Must(template.New("delete").ParseFiles("view.html"))
+		var err = tmpl.Execute(w, nil)
+
+		if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return
+	}
+	if r.Method == "POST"{
+
+		db, err := connect()
+    if err != nil {
+        log.Fatal(err.Error())
+	}
+	
+    var tmpl = template.Must(template.New("result_d").ParseFiles("view.html"))
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	
+	var vector = r.FormValue("data_d")
+	var selector = bson.M{"name": vector}
+
+	var data = map[string]string{"data": vector}
+
+	_, err = db.Collection("student").DeleteOne(ctx, selector)
+	if err != nil {
+        log.Fatal(err.Error())
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Println("Remove Success !")
+return
+	}
 }
 
 func updateEvent(w http.ResponseWriter, r *http.Request) {
-	
-	if r.Method== "PATCH"{
+	if r.Method== "GET"{
+		var tmpl = template.Must(template.New("update").ParseFiles("view.html"))
+		var err = tmpl.Execute(w, nil)
 
-	db, err := connect()
-	if err != nil {
-		log.Fatal(err.Error())
+		if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+        return	
 	}
 
-	_, err = db.Collection("student").UpdateOne(ctx, selector, bson.M{"$set": changes})
+	if r.Method== "POST"{
+
+		db, err :=connect()
 	if err != nil {
-		log.Fatal(err.Error())
+        log.Fatal(err.Error())
 	}
 
-}}
+	var tmpl = template.Must(template.New("result_u").ParseFiles("view.html"))
 
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
+	var vector = r.FormValue("data_u")
+	var selector = bson.M{"name": vector}
+
+	var name = r.FormValue("name_u")
+	var bubble = r.Form.Get("grade_u")
+	grade, err := strconv.Atoi(bubble)
+	if err == nil {
+		fmt.Printf("%T, %v\n", grade, grade)
 	}
+	var category = r.Form.Get("category_u")
+	var changes = student{name, grade, category}
+
+	var data = map[string]string{"name": name, "category": category }
+	var angka = map[string]int{ "grade": grade}
+	
+	_, err = db.Collection("student").UpdateOne(ctx, selector, bson.M{"$set": changes})
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		if err := tmpl.Execute(w, angka); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	return
+	}
+	http.Error(w, "", http.StatusBadRequest)
 }
 
-func main() {
+
+
+func main(){
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/read", readEvent)
+	router.HandleFunc("/event", createEvent).Methods("GET")
 	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	router.HandleFunc("/process", createEvent).Methods("POST")
-	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/update}", updateEvent).Methods("PATCH")
-	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/update", updateEvent).Methods("GET")
+	router.HandleFunc("/update", updateEvent).Methods("POST")
+	router.HandleFunc("/delete", deleteEvent).Methods("GET")
+	router.HandleFunc("/delete", deleteEvent).Methods("POST")
 	log.Fatal(http.ListenAndServe(":6060", router))
 }
